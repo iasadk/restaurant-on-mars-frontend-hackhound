d8cce65630968ccbd85b63d8f825555f336ebb4a
iasadk <89456039+iasadk@users.noreply.github.com>
2023-03-04 23:10:30 +0530

    Added Login and registration system with JWT token.

---
+++ src/service/user.js
@@ -0,0 +1,19 @@
+import axios from "../config/axios";
+
+export default class service {
+
+    // static baseURL = 'user';
+
+    static detailUser(id, data) {
+        return axios.get('/details-user/' + id, { params: data });
+    }
+    static saveUser(data, query) {
+        return axios.post("/signup", data, { params: query });
+    }
+    static login(data, query) {
+        return axios.post("/signin", data, { params: query });
+    }
+    static deleteUser(id) {
+        return axios.post('/delete-user', { ids: id });
+    }
+}

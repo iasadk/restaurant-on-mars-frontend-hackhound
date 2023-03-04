31cc161d0e9e60d212b0427f09a2032b41c78aa8
iasadk <89456039+iasadk@users.noreply.github.com>
2023-03-04 19:55:47 +0530

    Made Login and signup page.

--- src/Layout/Routes.js
+++ src/Layout/Routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
 import Menu from "../Pages/Menu";
+import SignUp from "../Pages/SignUp";
 
 const RoutesPro = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/menu" element={<Menu />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };

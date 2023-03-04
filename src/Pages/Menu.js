c8202ac48d5dc9a1026f3999dca632748a2336d5
Nitin Kumar <nkthehustler@gmail.com>
2023-03-05 05:23:53 +0530

    added menu page

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,8 +1,159 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <section class="text-gray-600 body-font">
+      <h1 className='text-center text-4xl my-5'>Menu</h1>
+  <div class="container px-10 py-7 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Pizza"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="pizzaic.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Pizzas</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Cuisines"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="bell.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Cuisines</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Drinks"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="coffee.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Drinks</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Desserts"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="muffin.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Desserts</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Specials"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="cupcake.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Special</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Italian"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="spaghetti.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Italian</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/Chinese"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="ramen.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Chinese</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <Link
+              to={"/FastFood"}
+              className="h-full flex items-center border-gray-200 border p-4 rounded-lg bg-[#424242] hover:bg-[#ff5432] transition duration-150 ease-out hover:ease-in"
+            >
+              <img
+                alt="team"
+                className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                src="burger.png"
+              />
+              <div className="flex-grow">
+                <p className="text-slate-50">Fast Food</p>
+              </div>
+            </Link>
+        </a>
+      </div>
+    
+    </div>
+  </div>
+</section>
   )
 }
 
--- src/Pages/Order.js
+++ src/Pages/Order.js
@@ -2,7 +2,82 @@ import React from 'react'
 
 const Order = () => {
   return (
-    <div>Order</div>
+    <div>
+      <h1 className='text-center text-4xl my-5'>Menu</h1>
+    <div className='bg-slate-100 p-4 my-12 w-full md:w-8/12 transition-all delay-100 ease-in-out mx-auto'>
+     
+    <div>
+        <div className=' mt-5 text-lg md:text-2xl font-semibold text-gray-400 flex justify-between'>
+            <p>Order ID:</p>
+            
+        </div>
+        <div className="flex flex-col md:flex-row justify-between text-sm mt-4">
+            <div className='flex gap-1 mb-2'>
+                <p>Order date:</p><span className='font-semibold text-md'></span>
+            </div>
+           
+        </div>
+
+        <div className='my-12'>
+                    <div  className='flex justify-between bg-slate-200 rounded my-4'>
+                        <div className='flex gap-2 my-4 '>
+                            <img src={"https://img.freepik.com/free-photo/spicy-salmon-salad_74190-647.jpg?size=626&ext=jpg"} alt="Tshirt black" className='w-1/4 md:w-1/6' width={500} height={500}/>
+                            <div className='text-slate-500 flex flex-col gap-3 md:justify-end'>
+                                <p className='text-lg font-normal '>pizza</p>
+                                <div className='flex gap-2 text-xs font-thin'>
+                                    <p>small</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex flex-col text-md w-[230px] justify-start md:justify-end items-start  my-4'>
+                            <p className='md:text-lg font-bold'>₹</p>
+                            <div className='text-sm mt-2'>
+                                <p className='text-slate-500 font-thin'>Qty: 1</p>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+
+        <div className='grid grid-cols-2 row-auto gap-y-3'>
+            <div className='payment'>
+                <h1 className='text-slate-600 flex items-center gap-1 font-semibold lg:text-lg'>Payment <i className="ri-currency-line"></i></h1>
+                <div className='mt-2'>
+                    <p className='text-md font-thin lg:text-base'>**56 <i className="ri-visa-line text-lg"></i></p>
+                </div>
+            </div>
+
+            <div>
+            </div>
+
+            <div>
+                <h1 className='text-slate-600 flex items-center gap-1 font-semibold lg:text-lg'>Need help <i className="ri-question-line"></i></h1>
+                <div>
+                    <p className='text-xs text-slate-500 font-thin my-2 flex items-center gap-1 xl:text-base'><i className="ri-questionnaire-fill"></i> Order Issue <i className="ri-arrow-right-up-line"></i></p>
+                    <p className='text-xs text-slate-500 font-thin my-2 flex items-center gap-1 xl:text-base'>
+                        <i className="ri-arrow-go-back-line"></i> Returns <i className="ri-arrow-right-up-line"></i>
+                    </p>
+                </div>
+                
+            </div>
+
+            <div>
+                <h1 className='text-slate-600 mt-1 flex items-center gap-1 border-t-2 border-slate-300 font-semibold lg:text-lg'>Order Summary</h1>
+                        <div className='flex justify-between items-center '>
+                            <p className='text-sm text-slate-500 font-thin my-2 lg:text-base'></p>
+                            <p>X ₹</p>
+                        </div>
+                <div className='flex justify-between items-center border-t-2 border-slate-300'>
+                    <p className='text-sm text-slate-500 font-thin my-2 lg:text-base'>Total</p>
+                    <p>₹40</p>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
   )
 }
 
